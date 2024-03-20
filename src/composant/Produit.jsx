b329ecc8b8d208d6805d90f8852d3c3d62ea1e34
Clavien317 <Claviennambinina511@gmail.com>
2024-03-20 20:07:49 +0300

    amelioration card

--- src/composant/Produit.jsx
+++ src/composant/Produit.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
-import { BsCurrencyDollar } from 'react-icons/bs'
+import { BsCurrencyDollar, BsEye } from 'react-icons/bs'
 import {FiTruck} from "react-icons/fi"
 import liste from "./produits"
+import {AiOutlineHeart, AiOutlineShoppingCart} from "react-icons/ai"
+
 
 
 function Produit() {
@@ -11,11 +13,10 @@ function Produit() {
 
   return (
     <>
-                <div className="prod_type">
+        <div className="prod_type">
             <div className="container">
                 <div className="box">
                     <div className="img_box"><img src="/4416.jpg" alt="" /></div>
-
                     <div className="detail">
                         <p>13 produits dispo</p>
                     </div>
@@ -25,8 +26,6 @@ function Produit() {
                     <div className="img_box">
                         <img src="/Telephones-double-SIM.jpg" alt="" />
                     </div>
-                   
-
                     <div className="detail">
                         <p>23 Produits dispo</p>
                     </div>
@@ -36,7 +35,6 @@ function Produit() {
                     <div className="img_box">
                         <img src="/Amazon-Fire-Max-11-Tablet-EMBED.webp" alt="" />
                     </div>
-
                     <div className="detail">
                         <p>23 Produits dispo</p>
                     </div>
@@ -46,7 +44,6 @@ function Produit() {
                     <div className="img_box">
                         <img src="/72f6611c323b4db28d3e40974c325c5e-image.webp" alt="" />
                     </div>
-
                     <div className="detail">
                         <p>12 Produits dispo</p>
                     </div>
@@ -54,7 +51,6 @@ function Produit() {
 
                 <div className="box">
                     <div className="img_box"><img src="/4416.jpg" alt="" /></div>
-
                     <div className="detail">
                         <p>13 produits dispo</p>
                     </div>
@@ -117,24 +113,36 @@ function Produit() {
             </div>
         </div>
 
-        {/* <div className="produits">
+        <div className="produits">
+            <h2>Nos meilleurs produits</h2>
+            <hr className='line' />
+            <br />
             <div className="container">
                 {
                     prod.map((item)=>
                     {
                         return(
-                        <div key={item.id}>
-                            <div className="img_box">
-                                <img src={item.image} alt="" />
-                            </div>
-                        </div>)
+                            <>
+                                <div className='box' key={item.id}>
+                                    <div className="img_box">
+                                        <img src={item.image} alt="" />
+                                        <div className="icon">
+                                            <li><AiOutlineShoppingCart /></li>
+                                            <li><BsEye /></li>
+                                            <li><AiOutlineHeart /></li>
+                                        </div>
+                                    </div>
+                                    <div className="detail">
+                                        <p>{item.categorie}</p>
+                                        <h3>{item.titre}</h3>
+                                        <h4>{item.prix}</h4>
+                                    </div>
+                                </div>
+                            </>)
                     })
                 }
-                <div className="box">
-
-                </div>
             </div>
-        </div> */}
+        </div>
     </>
   )
 }
--- src/composant/produits.js
+++ src/composant/produits.js
@@ -5,27 +5,27 @@ const liste =
         titre: "Iphone 13 pro max",
         categorie:"Smartphone",
         prix:"$2000",
-        image:"/4416.jpg"
+        image:"/raw.jpeg"
     },
     {
         id:2,
         titre: "TECNO SPARK",
         categorie:"Smartphone",
-        prix:"$1200",
+        prix:"$700",
         image:"/Telephones-double-SIM.jpg"
     },
     {
         id:3,
         titre: "Iphone ",
         categorie:"Tablet",
-        prix:"$1000",
+        prix:"$900",
         image:"/Amazon-Fire-Max-11-Tablet-EMBED.webp"
     },
     {
         id:4,
         titre: "SAMSUNG S20",
         categorie:"Smartphone",
-        prix:"$2000",
+        prix:"$600",
         image:"/72f6611c323b4db28d3e40974c325c5e-image.webp"
     },
     {
